75f3323380bf9a3529e4dba0d7223a5f4d4120fa
taranaga <taranaga@gmail.com>
2016-08-20 19:44:59 +0300

    solution was created. Also task description was added.

---
+++ SoftServeTask/SoftServeTask/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftServeTask
+{
+    /// <summary>
+    /// Create next classes: Cat, Dog and Petshop.Types Cat and Dog have fields Name and Breed and method Introduce() which prints
+    /// text “I’m (Name) of (Breed). I’m a cat (or dog)”.The class Petshop collects in its container different pets.We may add new
+    /// pet to container by method AddPet() and we may display information about all pets by method IntroduceAll().
+    /// Which hierarchy of classes is the best solution of this problem? Write short code to demonstrate your solution.
+    /// Your code should include class (interface) aggregation, inheritance, should use .NET BCL collections or generics,
+    /// should implement exception handling.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
